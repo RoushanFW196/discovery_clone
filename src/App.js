be872fe19aad6f30f9504ae7286771bd0355be0f
Alimul Hassan <alhassan069@gmail.com>
2022-02-24 02:27:18 +0530

    added details page

--- src/App.js
+++ src/App.js
@@ -1,31 +1,48 @@
-import './App.css'
-import { Navbar } from './components/navbar/Navbar'
-import { Login } from './components/login/Login'
-import { Signup } from './components/signup/Signup'
-import { Route, Routes } from 'react-router-dom'
-import { Home } from './components/home/Home'
-import { Explore } from './components/explore/Explore'
-import { Kids } from './components/kids/Kids'
-import { Mindblown } from './components/mindblown/Mindblown'
-import { Premium } from './components/premium/Premium'
+import "./App.css";
+import { Navbar } from "./components/navbar/Navbar";
+import { Login } from "./components/login/Login";
+import { Signup } from "./components/signup/Signup";
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./components/home/Home";
+import { Explore } from "./components/explore/Explore";
+import { Kids } from "./components/kids/Kids";
+import { Mindblown } from "./components/mindblown/Mindblown";
+import { Premium } from "./components/premium/Premium";
+import { Details } from "./components/detailsPage/Details";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home/>}></Route>
-        <Route path='/explore' element={<Explore/>}></Route>
-        <Route path='/kids' element={<Kids/>}></Route>
-        <Route path='/mindblown' element={<Mindblown/>}></Route>
-        <Route path='/premium' element={<Premium/>}></Route>
-        <Route path='/login' element={<Login />}></Route>
-        <Route path='/signup' element={<Signup />}></Route>
-      </Routes>
-      {/* <Login /> */}
-      {/* <Signup /> */}
+        <Route path="/" element={<Home />}>
+          {" "}
+        </Route>{" "}
+        <Route path="/explore" element={<Explore />}>
+          {" "}
+        </Route>{" "}
+        <Route path="/kids" element={<Kids />}>
+          {" "}
+        </Route>{" "}
+        <Route path="/mindblown" element={<Mindblown />}>
+          {" "}
+        </Route>{" "}
+        <Route path="/premium" element={<Premium />}>
+          {" "}
+        </Route>{" "}
+        <Route path="/login" element={<Login />}>
+          {" "}
+        </Route>{" "}
+        <Route path="/signup" element={<Signup />}>
+          {" "}
+        </Route>{" "}
+        <Route path="/details" element={<Details />}>
+          {" "}
+        </Route>{" "}
+      </Routes>{" "}
+      {/* <Login /> */} {/* <Signup /> */}{" "}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
