beb1a063a41011c3a278c7ab9eb1af7f288a377d
mohsin19295 <mohsin19295@gmail.com>
2022-02-25 15:01:11 +0530

    footer added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Premium from './components/premium/Premium';
+import Footer from "./components/footer/Footer.js"
 
 function App() {
   return (
     <div className="App">
      <Premium/>
+     <Footer/>
     </div>
   );
 }
