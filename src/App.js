1d4e4e21a5f129a904225289fb95db4708ee4f69
mohsin19295 <mohsin19295@gmail.com>
2022-02-25 19:01:16 +0530

    mindblown added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Premium from './components/premium/Premium';
 import Footer from "./components/footer/Footer.js"
+import Mindblown from "./components/mindblown/Mindblown.js"
 
 function App() {
   return (
     <div className="App">
      <Premium/>
      <Footer/>
+     <Mindblown/>
     </div>
   );
 }
